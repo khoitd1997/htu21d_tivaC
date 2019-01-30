9e79937d47cfdb31c765b72c7e781b525be06d91
khoitd1997 <khoidinhtrinh@gmail.com>
2019-01-29 22:02:44 -0800

    docs: Added doxygen and README docs

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # htu21d_tivaC
-Port of the htu21d driver for the tivaC
+
+Port of the htu21d driver for the tivaC, this is customized for personal use so definitely read the code carefully before you use it.
+
+## Design
+
+I try to separate the logic from the specific i2c details of the tivaC so that I can port this driver to other mcu in the future, the htu21d_i2c header and source file will be where most of the tivaC i2c specific goes while the htu21d.c will contain mostly logic specific to the htu21d.
+
+## Credits
+
+I copied most of sparkfun macros for register definition [here][1], the rest is just implementation of the datasheet.
+
+## Features
+
+- No hold data read for temperature and humidity
+- Resolution read/write
+- Software reset
+
+## Directory Structure
+
+- include/:
+  - htu21d.h: function prototypes of main features, also contain Doxygen docs
+- src/: source code of the driver
+  - htu21d_hw.h: register macros and other constants specific to the htu21d
+  - htu21d_i2c.c and .h: i2c helper layer specific to the tivaC
+  - htu21d.c: implementation of logic specific to the htu21d
+  - main.c: contain example of how to use the implemented features
+
+Other directory will belong to Code Composer Studio project.
+
+[1]:https://github.com/sparkfun/SparkFun_HTU21D_Breakout_Arduino_Library/blob/master/src/SparkFunHTU21D.h
--- include/htu21d.h
+++ include/htu21d.h
@@ -7,6 +7,10 @@ extern "C" {
 
 #include <stdint.h>
 
+/**
+ * @brief possible resolutions for both humidity and temp
+ *
+ */
 typedef enum {
   HTU21D_HUMIDITY_12_TEMP_14 = 0x00,
   HTU21D_HUMIDITY_8_TEMP_12  = 0x01,
@@ -14,22 +18,93 @@ typedef enum {
   HTU21D_HUMIDITY_11_TEMP_11 = 0x81,
 } Htu21d_resolution;
 
-typedef enum { HTU21D_ERROR_NONE = 0, HTU21D_ERROR_NO_DATA, HTU21D_ERROR_COMM } Htu21d_error;
+/**
+ * @brief error return for htu
+ *
+ */
+typedef enum {
+  HTU21D_ERROR_NONE = 0,  ///< no error
+  HTU21D_ERROR_NO_DATA,   ///< no data
+  HTU21D_ERROR_COMM       ///< general error
+} Htu21d_error;
 
+/**
+ * @brief represents an htu21d device, needed for all operations
+ *
+ */
 typedef struct {
-  uint32_t          i2c_base;
-  Htu21d_resolution res;
+  uint32_t          i2c_base;  ///< base addr of the i2c controller like I2C0_BASE
+  Htu21d_resolution res;       ///< current resolution
 } Htu21d;
 
+/**
+ * @brief prepare the i2c clock, gpio clock and pins
+ * This function is hard coded because the amount of params needed is pretty high
+ *
+ * @param htu device struct pointer
+ */
 void htu21d_init(const Htu21d *htu);
 
+/**
+ * @brief read and store the current resolution of device into the htu struct
+ *
+ * @param htu device struct pointer
+ * @return Htu21d_error
+ */
 Htu21d_error htu21d_read_resolution(Htu21d *htu);
+
+/**
+ * @brief write new resolution to device and update device struct
+ *
+ * @param htu device struct pointer
+ * @param res the target resolution
+ * @return Htu21d_error
+ */
 Htu21d_error htu21d_write_resolution(Htu21d *htu, const Htu21d_resolution res);
 
-void         htu21d_start_humid_read(const Htu21d *htu);
+/**
+ * @brief ask the sensor to start reading humidity
+ * Time until results are available depends on the sampling mode
+ *
+ * @param htu device struct pointer
+ */
+void htu21d_start_humid_read(const Htu21d *htu);
+
+/**
+ * @brief check if the sensor has finished sampling humidity
+ *
+ * @param htu device struct pointer
+ * @param output contain the newest humidity reading if there is no error
+ * @return Htu21d_error can be none if data available, no data if sensor is not ready or other
+ * errors depending on issue
+ */
 Htu21d_error htu21d_check_humid_read(const Htu21d *htu, float *output);
-void         htu21d_start_temp_read(const Htu21d *htu);
+
+/**
+ * @brief ask the sensor to start reading temperature
+ * Time until results are available depends on the sampling mode
+
+ * @param htu device struct pointer
+ */
+void htu21d_start_temp_read(const Htu21d *htu);
+
+/**
+ * @brief check if the sensor has finished sampling temperature
+ * Time until results are available depends on the sampling mode
+ *
+ * @param htu device struct pointer
+ * @param output contain the newest temperature reading if there is no error
+ * @return Htu21d_error can be none if data available, no data if sensor is not ready or other
+ * errors depending on issue
+ */
 Htu21d_error htu21d_check_temp_read(const Htu21d *htu, float *output);
+
+/**
+ * @brief perform a software reset on the sensor
+ *
+ * @param htu device struct pointer
+ */
+void htu21d_soft_reset(const Htu21d *htu);
 
 #ifdef __cplusplus
 }
